259eb8031a690132cc61d5701793012c72356523
itzrilly <rilwanougarbarilly@gmail.com>
2022-06-17 17:01:14 +0100

    Bottom tabs bar  updated

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -2,125 +2,105 @@ import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
-import { BottomTabBar, createBottomTabNavigator } from 'react-navigation-tabs'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import Login from '../Components/Login'
 import Home from '../Components/Home'
 import Product from '../Components/Product'
 import Service from '../Components/Service'
 import Support from '../Components/Support'
 import Menu from '../Components/Menu'
 
-const HomeStackNavigator = createStackNavigator({
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        title: 'Accueil'
-      }
-    }
-})
-
-const ProductStackNavigator = createStackNavigator({
-    Product: {
-      screen: Product,
-      navigationOptions: {
-        title: 'Produit'
-      }
-    }
-})
-
-const ServiceStackNavigator = createStackNavigator({
-    Service: {
-      screen: Service,
-      navigationOptions: {
-        title: 'Service'
-      }
-    }
-})
-
-const SupportStackNavigator = createStackNavigator({
-    Support: {
-      screen: Support,
-      navigationOptions: {
-        title: 'Support'
-      }
-    }
-})
-
-const MenuStackNavigator = createStackNavigator({
-    Menu: {
-      screen: Menu,
-      navigationOptions: {
-        title: 'Menu'
-      }
-    }
-})
-
-const BlueTabNavigator = createBottomTabNavigator(
-    {
-        Home: {
-            screen: HomeStackNavigator,
-            navigationOptions: {
-              tabBarIcon: () => {
-                  return <Image source={require('../Images/ic_home.png')}
-                    style={styles.icon}/>
-                }
-            }
-        },
-        Product: {
-            screen: ProductStackNavigator,
-            navigationOptions: {
-              tabBarIcon: () => {
-                  return <Image source={require('../Images/ic_product.png')}
-                    style={styles.icon}/>                 
-                },
-            }
-        },
-        Service: {
-          screen: ServiceStackNavigator,
-          navigationOptions: {
-            tabBarIcon: () => {
-              return <Image  source={require('../Images/ic_service.png')}
-                style={styles.icon} />                    
-            }
-          }
-        },
-        Support: {
-          screen: SupportStackNavigator,
-          navigationOptions: {
-            tabBarIcon: () => {
-              return <Image  source={require('../Images/ic_support.png')}
-                style={styles.icon} />                
-            }
-          }
-        },
-        Menu: {
-            screen: MenuStackNavigator,
-            navigationOptions: {
-              tabBarIcon: () => {
-                return <Image source={require('../Images/ic_menu.png')}
-                    style={styles.icon} />
-                }
-            }
-          }
-      },
-        {
-            tabBarOptions: {
-                activeBackgroundColor: '#DDDDDD',
-                inactiveBackgroundColor: '#FFFFFF',
-                showLabel: true,
-                showIcon: true,
-                style:{
-                    borderTopWidth: 1,
-                    borderTopColor: '#D3D3D3'
-                }
+const BlueTabNavigator = createBottomTabNavigator({
+    Home: { screen: Home },
+    Product: { screen: Product },
+    Service: { screen: Service },
+    Support: { screen: Support },
+    Menu: { screen: Menu }
+},
+{
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) => {
+            const { routeName } = navigation.state
+            if(routeName === 'Home'){
+                return (
+                    <View style={{ flex: 1, justifyContent: 'flex-end', borderTopColor: focused ? tintColor : '', borderTopWidth: focused ? 2 : 0 }}>
+                        <Image 
+                            source={require('../Images/ic_home.png')}
+                            style={styles.icon} />
+                    </View>
+                )
+            }else if(routeName === 'Product'){
+                return (
+                    <View style={{ flex: 1, justifyContent: 'flex-end', borderTopColor: focused ? tintColor : '', borderTopWidth: focused ? 2 : 0 }}>
+                        <Image 
+                            source={require('../Images/ic_product.png')}
+                            style={styles.icon} />
+                    </View>  
+                ) 
+            }else if(routeName === 'Service'){
+                return (
+                    <View style={{ flex: 1, justifyContent: 'flex-end', borderTopColor: focused ? tintColor : '', borderTopWidth: focused ? 2 : 0 }}>
+                        <Image  
+                            source={require('../Images/ic_service.png')}
+                            style={styles.icon} />   
+                    </View>
+                )
+            }else if(routeName === 'Support'){
+                return (
+                    <View style={{ flex: 1, justifyContent: 'flex-end', borderTopColor: focused ? tintColor : '', borderTopWidth: focused ? 2 : 0 }}>
+                        <Image  
+                            source={require('../Images/ic_support.png')}
+                            style={styles.icon} />     
+                    </View>
+                )
+            }else{
+                return (
+                    <View style={{ flex: 1, justifyContent: 'flex-end', borderTopColor: focused ? tintColor : '', borderTopWidth: focused ? 2 : 0 }}>
+                        <Image 
+                            source={require('../Images/ic_menu.png')}
+                            style={styles.icon} />
+                    </View>
+                )
             }
         }
-  )
+    }),
+    tabBarOptions: {
+        activeTintColor: 'blue',
+        inactiveTintColor: 'rgba(0,0.0,0,6)',
+        showLabel: true,
+        showIcon: true,
+        style: {
+            shadowColor: 'rgba(0,0.0,0,6)',
+            showLabel: { width: 0, height: 0 },
+            shadowOpacity: 0.1,
+            shadowRadius: 15,
+            elevation: 5,
+            borderTopColor: 'transparent',
+            backgroundColor: '#fff',
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20
+        }
+    }
+})
+
+const Navigation = createStackNavigator({
+    Login: { screen: Login },
+    Home: { screen: BlueTabNavigator },
+    Product: { screen: Product },
+    Service: { screen: Service },
+    Support: { screen: Support },
+    Menu: { screen: Menu }
+},
+{
+    headerMode: 'none',
+    initialRouteName: 'Home'
+})
   
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     icon: {
       width: 23,
       height: 23
     }
-  })
-  
-export default createAppContainer(BlueTabNavigator)
+})
+
+export default createAppContainer(Navigation)
