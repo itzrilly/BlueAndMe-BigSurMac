fb21818f185d76958f34af4039388d10668e4b9c
itzrilly <rilwanougarbarilly@gmail.com>
2022-06-17 09:47:45 +0100

    Bottom Tab Bar updated: Menu Bar added

---
+++ Components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Menu(){
+    
+    return(
+        <View>
+            <Text>Menu View</Text>
+        </View>
+    )
+
+}
+
+export default Menu
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { BottomTabBar, createBottomTabNavigator } from 'react-navigation-tabs'
 import Home from '../Components/Home'
 import Product from '../Components/Product'
 import Service from '../Components/Service'
 import Support from '../Components/Support'
+import Menu from '../Components/Menu'
 
 const HomeStackNavigator = createStackNavigator({
     Home: {
@@ -18,7 +19,7 @@ const HomeStackNavigator = createStackNavigator({
 })
 
 const ProductStackNavigator = createStackNavigator({
-    Favorites: {
+    Product: {
       screen: Product,
       navigationOptions: {
         title: 'Produit'
@@ -27,7 +28,7 @@ const ProductStackNavigator = createStackNavigator({
 })
 
 const ServiceStackNavigator = createStackNavigator({
-    News: {
+    Service: {
       screen: Service,
       navigationOptions: {
         title: 'Service'
@@ -36,10 +37,19 @@ const ServiceStackNavigator = createStackNavigator({
 })
 
 const SupportStackNavigator = createStackNavigator({
-    WatchedFilm: {
+    Support: {
       screen: Support,
       navigationOptions: {
         title: 'Support'
+      }
+    }
+})
+
+const MenuStackNavigator = createStackNavigator({
+    Menu: {
+      screen: Menu,
+      navigationOptions: {
+        title: 'Menu'
       }
     }
 })
@@ -50,38 +60,26 @@ const BlueTabNavigator = createBottomTabNavigator(
             screen: HomeStackNavigator,
             navigationOptions: {
               tabBarIcon: () => {
-                  return <View style={styles.ic_container}>
-                    <Image
-                        source={require('../Images/ic_home.png')}
-                        style={styles.icon}/>
-                    <Text style={styles.ic_name}>Accueil</Text>
-                </View>
-              }
+                  return <Image source={require('../Images/ic_home.png')}
+                    style={styles.icon}/>
+                }
             }
         },
         Product: {
             screen: ProductStackNavigator,
             navigationOptions: {
               tabBarIcon: () => {
-                  return <View style={styles.ic_container}>
-                    <Image
-                        source={require('../Images/ic_product.png')}
-                        style={styles.icon}/>
-                    <Text style={styles.ic_name}>Produit</Text>
-                </View>                    
-              },
+                  return <Image source={require('../Images/ic_product.png')}
+                    style={styles.icon}/>                 
+                },
             }
         },
         Service: {
           screen: ServiceStackNavigator,
           navigationOptions: {
             tabBarIcon: () => {
-              return <View style={styles.ic_container}>
-                <Image
-                    source={require('../Images/ic_service.png')}
-                    style={styles.icon} />
-                    <Text style={styles.ic_name}>Service</Text>
-                </View> 
+              return <Image  source={require('../Images/ic_service.png')}
+                style={styles.icon} />                    
             }
           }
         },
@@ -89,38 +87,39 @@ const BlueTabNavigator = createBottomTabNavigator(
           screen: SupportStackNavigator,
           navigationOptions: {
             tabBarIcon: () => {
-              return <View style={styles.ic_container}>
-               <Image
-                    source={require('../Images/ic_support.png')}
-                    style={styles.icon} />
-                    <Text style={styles.ic_name}>Support</Text>
-                </View> 
+              return <Image  source={require('../Images/ic_support.png')}
+                style={styles.icon} />                
             }
           }
-        }
+        },
+        Menu: {
+            screen: MenuStackNavigator,
+            navigationOptions: {
+              tabBarIcon: () => {
+                return <Image source={require('../Images/ic_menu.png')}
+                    style={styles.icon} />
+                }
+            }
+          }
       },
         {
-        tabBarOptions: {
-            activeBackgroundColor: '#DDDDDD',
-            inactiveBackgroundColor: '#FFFFFF',
-            showLabel: false,
-            showIcon: true
-        }
+            tabBarOptions: {
+                activeBackgroundColor: '#DDDDDD',
+                inactiveBackgroundColor: '#FFFFFF',
+                showLabel: true,
+                showIcon: true,
+                style:{
+                    borderTopWidth: 1,
+                    borderTopColor: '#D3D3D3'
+                }
+            }
         }
   )
   
   const styles = StyleSheet.create({
-    ic_container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
     icon: {
-      width: 25,
-      height: 25
-    },
-    ic_name: {
-        fontSize: 12
+      width: 23,
+      height: 23
     }
   })
   
