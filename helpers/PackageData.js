e42d5c4db9044ed725f0d088f473d376f063ffc0
itzrilly <rilwanougarbarilly@gmail.com>
2022-06-30 15:18:23 +0100

    Test ressources files added

---
+++ helpers/PackageData.js
@@ -0,0 +1,117 @@
+export default data =  [
+    {
+        infoPackage: [
+            { id: 117, category: 'Forfaits tout-en-un'},
+            { id: 1, title: 'TOLI XS', price: '500', gain: '500Mo', overview: '50 SMS, 50 min appels tous réseaux', validity: '24H'},
+            { id: 2, title: 'TOLI S', price: '2000', gain: '2Go', overview: '200 minutes appels  + 200 SMS tous réseaux', validity: '30 jours'},
+            { id: 3, title: 'TOLI M', price: '5000', gain: '5Go', overview: '500 SMS, 500 min appels tous réseaux', validity: '30 jours'},
+            { id: 4, title: 'TOLI L', price: '10000', gain: '10Go', overview: '1000 SMS, 1 000 min appels tous réseaux',  validity: '30 jours'},
+            { id: 5, title: 'TOLI XL', price: '20000', gain: '20Go', overview: '2000 SMS, 2 000 min appels tous réseaux', validity: '30 jours'},
+            { id: 6, title: 'TOLI XXL', price: '35000', gain: '35Go', overview: '3500 SMS, 3500 min appels tous réseaux',  validity: '30 jours'},
+
+            { id: 118, category: 'Internet uniquement'},
+
+            { id: 119, daily: 'Journalier'},
+            { id: 7, title: "X-DAY", price: "1000", gain: "1,5Go", validity: "24H"},
+            { id: 8, title: "SPOT", price: "100", gain: "500Mo", validity: "4h"},
+            { id: 9, title: "COOL", price: "300", gain: "1.5Go", validity: "24H"},
+            { id: 10, title: "FAP", price: "500", gain: "2Go", validity: "48H"},
+            { id: 11, title: "FAKO XS", price: "500", gain: "2Go", validity: "24H"},
+            // Blue 1
+            // { id: 53, category_less: "Blue 1 Journalier"},
+            { id: 36, title: "Blue 1 Booster", price: "500", gain: "2Go", validity: "24H"},
+            
+            { id: 121, weekly: 'Hebdomadaire'},
+            { id: 12, title: "X-WEEK", price: "5000",  gain: "12Go", validity: "7 jours"},
+            { id: 13, title: "KOLO", price: "1000", gain: "2Go", validity: "7 jours"},
+            { id: 14, title: "DEAL", price: "3000", gain: "5Go", validity: "7 jours"},
+            // Blue 1
+            // { id: 54, category_less: "Blue 1"},
+            { id: 31, title: "Blue 1 S", price: "2000U", gain: "4Go", validity: "7 jours", 
+                ic_facebook: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_facebook.png',
+                ic_whatsapp: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_whatsapp.png',
+                ic_twitter: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_twitter.png',
+                ic_telegram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_telegram.png',
+                all_airtime_network: '4000'
+            },
+
+            { id: 123, monthly: 'Mensuel'},
+            { id: 15, title: 'X-STARTER',  price: '10000', gain: '20Go', validity: '30 jours'},
+            { id: 16, title: 'X-PREMIUM', price: '20000', gain: '50Go', validity: '30 jours'},
+            { id: 17, title: 'X-BUSINESS',  price: '35000',  gain: '100Go', validity: '30 jours'},
+            { id: 18, title: 'X-TREM UNLIMITED', price: '900000', gain: 'Go illimités', validity: '30 jours'},
+            { id: 19, title: 'SWIM', price: '5000', gain: '10Go', validity: '30 jours'},
+            { id: 20, title: 'SKATE', price: '10000',  gain: '20Go', validity: '30 jours'},
+            { id: 21, title: 'PREMIUM', price: '20000', gain: '50Go', validity: '30 jours'},
+            { id: 22, title: 'ENJOY', price: '35000', gain: '100Go', validity: '30 jours'},
+            /*{ id: 23, title: 'Blue GO S', price: '', gain: '', validity: '30 jours'},
+            { id: 24, title: 'Blue GO M', price: '', gain: '', validity: '30 jours'},
+            { id: 25, title: 'Blue GO L', price: '', gain: '', validity: '30 jours'},
+            { id: 26, title: 'Blue GO XL',  price: '', gain: '', validity: '30 jours'},
+            { id: 27, title: 'Blue GO XXL',  price: '', gain: '', validity: '30 jours'},
+            { id: 28, title: 'Blue GO Plus S', price: '', gain: '', validity: '30 jours'},
+            { id: 29, title: 'Blue GO Plus M', price: '',  gain: '', validity: '30 jours'},
+            { id: 30, title: 'Blue GO Plus L', price: '', gain: '', validity: '30 jours'},*/
+            // Blue 1
+            { id: 55, category_less: "Blue 1"},
+            { id: 32, title: "Blue 1 M", price: "3000", gain: "4,5Go", validity: "30 jours",
+                ic_facebook: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_facebook.png',
+                ic_whatsapp: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_whatsapp.png',
+                ic_twitter: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_twitter.png',
+                ic_telegram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_telegram.png',
+                all_airtime_network: '6000'
+            },
+            { id: 33, title: "Blue 1 L", price: "3000", gain: "4,5Go", validity: "30 jours", 
+                ic_facebook: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_facebook.png',
+                ic_whatsapp: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_whatsapp.png',
+                ic_twitter: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_twitter.png',
+                ic_telegram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_telegram.png',
+                ic_instagram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_instagram.png',
+                all_airtime_network: '10000'
+            },
+            { id: 34, title: "Blue 1 XL", price: "3000", gain: "4,5Go", validity: "30 jours", 
+                ic_facebook: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_facebook.png',
+                ic_whatsapp: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_whatsapp.png',
+                ic_twitter: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_twitter.png',
+                ic_telegram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_telegram.png',
+                ic_instagram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_instagram.png',
+                ic_youtube: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_youtube.png',
+                all_airtime_network: '20000'
+            },
+            { id: 35, title: "Blue 1 XXL", price: "3000", gain: "4,5Go", validity: "30 jours", 
+                ic_facebook: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_facebook.png',
+                ic_whatsapp: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_whatsapp.png',
+                ic_twitter: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_twitter.png',
+                ic_telegram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_telegram.png',
+                ic_instagram: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_instagram.png',
+                ic_youtube: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_youtube.png',
+                ic_netflix: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_netflix.png',
+                all_airtime_network: '40000'
+            },
+            // Blue nous
+            { id: 56, category_less: "Blue Nous Plans"},
+            { id: 37, title: "Blue nous S", price: "2000", gain: "2Go", validity: "30 jours", call_flot: 'Illimités', sms_flot: 'Illimités', all_airtime_network: '2000', all_sms_network: '200', airtime_other_blue: '8000'},
+            { id: 38, title: "Blue nous M", price: "5000", gain: "5Go", validity: "30 jours", call_flot: 'Illimités', sms_flot: 'Illimités', all_airtime_network: '5000', all_sms_network: '500', airtime_other_blue: '15000'},
+            { id: 39, title: "Blue nous L", price: "10000", gain: "10Go", validity: "30 jours", call_flot: 'Illimités', sms_flot: 'Illimités', all_airtime_network: '10000', all_sms_network: '1000', airtime_other_blue: '40000'},
+            { id: 40, title: "Blue nous XL", price: "20000", gain: "20Go", validity: "30 jours", call_flot: 'Illimités', sms_flot: 'Illimités', all_airtime_network: '20000', all_sms_network: '2000', airtime_other_blue: '80000'},
+            { id: 41, title: "Blue nous XXL", price: "35000", gain: "35Go", validity: "30 jours", call_flot: 'Illimités', sms_flot: 'Illimités', all_airtime_network: '35000', all_sms_network: '3500', airtime_other_blue: '140000'},
+            // Blue go Plus de données
+            { id: 57, category_less: "Blue GO Plus de données"},
+            { id: 42, title: "Blue GO S", price: "2000", gain: "9Go", validity: "30 jours", daily_limit: '300Mo'},
+            { id: 43, title: "Blue GO M", price: "5000", gain: "25,5Go", validity: "30 jours", daily_limit: '850Mo'},
+            { id: 44, title: "Blue GO L", price: "10000", gain: "60Go", validity: "30 jours", daily_limit: '2Go'},
+            { id: 45, title: "Blue GO XL", price: "20000", gain: "135Go", validity: "30 jours", daily_limit: '4,5Go'},
+            { id: 46, title: "Blue GO XXL", price: "35000", gain: "240Go", validity: "30 jours", daily_limit: '8Go'},
+            // Blue go+ Encore plus de données
+            { id: 58, category_less: "Blue GO Encore plus de données"},
+            { id: 47, title: "Blue GO+ S", price: "15000", gain: "60Go", validity: "30 jours"},
+            { id: 48, title: "Blue GO+ M", price: "25000", gain: "135Go", validity: "30 jours"},
+            { id: 49, title: "Blue GO+ L", price: "40000", gain: "240Go", validity: "30 jours"},
+            // Blue Home, Haute vitesse, Illimité, Sans installation
+            { id: 59, category_less: "Blue Home, Haute vitesse, Illimité, Sans installation"},
+            { id: 50, title: "Blue Home S", price: "15000", gain: "Jusqu'à 04Mb/s", download: 'Illimités', validity: "30 jours"},
+            { id: 51, title: "Blue Home M", price: "25000", gain: "Jusqu'à 07Mb/s", download: 'Illimités', validity: "30 jours"},
+            { id: 52, title: "Blue Home L", price: "40000", gain: "Jusqu'à 10Mb/s", download: 'Illimités', validity: "30 jours"},
+        ]
+    }
+]
---
+++ helpers/StatData.js
@@ -0,0 +1,10 @@
+export default data =  [
+    {
+        infoBalance: [
+            { id: 1, title: 'CREDIT PRINCIPAL', image: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_appel_sortant.png', qty: '10000', text_validity: 'Date limite d\'accès au réseau:', date_exp: '01-01-2037' },
+            { id: 2, title: 'DATA 4G', image: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_antenne_relais_colored.png', data: '950Mo', text_validity: 'Validité:', date_exp: '01-10-2022' },
+            { id: 3, title: 'SMS', image: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_sms.png', sms: '100 SMS', text_validity: 'Validité:', date_exp: '01-10-2022' },
+            { id: 4, title: 'BONUS TOUT RESEAU', image: 'https://raw.githubusercontent.com/itzrilly/BlueAndMe/main/assets/images/icons/ic_microphone.png', qty: '1000', text_validity: 'Validité:', date_exp: '01-10-2022' }
+       ]
+    }
+]
