d78e02749b1341866d11cbd559d2753b575e3bae
itzrilly <rilwanougarbarilly@gmail.com>
2022-07-06 11:54:27 +0100

    PackagesData divided in two files to facilitate showing the packages

---
+++ helpers/Aio.js
@@ -0,0 +1,13 @@
+export default data =  [
+    {
+        infoPackage: [
+            { id: 117, category: 'Forfaits tout-en-un'},
+            { id: 1, title: 'TOLI XS', price: '500', gain: '500Mo', overview: '50 SMS, 50 min appels tous réseaux', validity: '24H'},
+            { id: 2, title: 'TOLI S', price: '2000', gain: '2Go', overview: '200 minutes appels  + 200 SMS tous réseaux', validity: '30 jours'},
+            { id: 3, title: 'TOLI M', price: '5000', gain: '5Go', overview: '500 SMS, 500 min appels tous réseaux', validity: '30 jours'},
+            { id: 4, title: 'TOLI L', price: '10000', gain: '10Go', overview: '1000 SMS, 1 000 min appels tous réseaux',  validity: '30 jours'},
+            { id: 5, title: 'TOLI XL', price: '20000', gain: '20Go', overview: '2000 SMS, 2 000 min appels tous réseaux', validity: '30 jours'},
+            { id: 6, title: 'TOLI XXL', price: '35000', gain: '35Go', overview: '3500 SMS, 3500 min appels tous réseaux',  validity: '30 jours'},
+        ]
+    }
+]
